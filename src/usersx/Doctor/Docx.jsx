350b4bf07248073fdaa0f4c02e0b1a9ca0e4fae8
shahidx0x <shahidabdulla.aas@gmail.com>
2022-02-03 20:34:14 +0600

    iniit commit

--- src/usersx/Doctor/Docx.jsx
+++ src/usersx/Doctor/Docx.jsx
@@ -1,19 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, FormControl, InputGroup } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import useAuth from "../../hooks/useAuth";
 import DocCard from "./DocCard";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useForm } from "react-hook-form";
 import AL from "../../component/ApiLoading/AL";
 
 const Docx = () => {
   const { user } = useAuth();
   const [morder, setMorder] = useState([]);
   const [filteredData, setFIlteredData] = useState([]);
-  const [srchData, setSrchData] = useState([]);
-  const { register, handleSubmit } = useForm();
-  const notify = () => toast.warn("No Result Found ");
   useEffect(() => {
     fetch("https://project-101-doctor.herokuapp.com/users-info")
       .then((res) => res.json())
@@ -24,17 +19,9 @@ const Docx = () => {
         );
       });
   }, [morder, filteredData, user.displayName]);
-  const onSubmit = (data) => {
-    xrs();
-  };
-  const xrs = () => {
-    setFIlteredData(
-      filteredData.filter((mor) => mor.Name.toLowerCase().includes("karma"))
-    );
-  };
+
   return (
     <Container className="mt-5">
-      <ToastContainer />
       <h2 className="text-center" style={{ fontSize: "50px" }}>
         Patient List
       </h2>
